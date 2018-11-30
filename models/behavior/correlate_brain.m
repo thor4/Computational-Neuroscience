276a8c24e7b4d2d4034f91fc7e309f146c3e5a93
Bryan Conklin <bryan.conklin4@gmail.com>
2018-11-29 22:56:27 -0500

    add part 3 for hw 5_6

--- models/behavior/correlate_brain.m
+++ models/behavior/correlate_brain.m
@@ -1,8 +1,12 @@
-% load('fmri_CST_data.mat')
+load('fmri_CST_data.mat') %load fmri synthetic "images"
 
 imagesc(img_sequence(:,:,1)), colormap(gray) %check it
 
-results_img=zeros(256,256); %store results
+results_img=zeros(256,256); %store results, pixel-by-pixel
+results_img_d=zeros(256,256); %store results, pixel-by-pixel for drift param
+
+%get activity prediction vector
+predict_activity
 
 for i=1:256
     for j=1:256
@@ -11,11 +15,19 @@ for i=1:256
         curr_vec=squeeze(img_sequence(i,j,:));
         
         %correlate predicted activity to actual brain activity
-        [r,p] = corr(curr_vec,activity_prediction_vector);
-        
+        [r_d,p_d] = corr(curr_vec,activity_prediction_vector); %drift
+        [r,p] = corr(curr_vec,activity_prediction_vector); %bias
+                
         results_img(i,j)=p;
+        results_img_d(i,j)=p_d;
         
     end
 end
 
-imagesc(results_img<.001) %show only the p<0.001 significant p values
+figure(1), clf
+imagesc(1:256,1:256,1e10.*(squeeze(mean(img_sequence,3)))), hold on %scale up so it survives the contour
+contour(1:256,1:256,results_img<.001,5,'linecolor','w') %show only the p<0.001 significant p values
+title('regions of brain that correlate w/ best-fit drift rate and bias params, p<0.001')
+export_fig bias_param_best_fit_p_001.pdf % export to pdf
+figure(2), clf %show drift, same as bias
+imagesc(results_img_d<.001) %show only the p<0.001 significant p values
--- models/behavior/predict_activity.m
+++ models/behavior/predict_activity.m
@@ -1,10 +1,10 @@
-%test the model with best-fit parameters, creates something that includes
+%test the model with best-fit parameters, creates data that includes
 %values derived from the model that change on a trial by trial basis
 
 %load up best drift and bias parameters based on model fit
 
-drift_parameters = [0.003 0.003 0.003 0.003];
-bias_parameters = [0.1 -0.5 0.2 -0.3];
+drift_parameters = [0.003 0.002 0.003 0.003];
+bias_parameters = [0.085 -0.281 0.472 -0.094];
 
 % conditions: hel go, hel change, lel go, lel change
 
@@ -12,22 +12,29 @@ trial_sequence=importdata('TrialLabels.txt'); %load up the sequences
 
 %look for string matches
 hel_go=ismember(trial_sequence,'High Error Likelihood/GO');
-plot(hel_go) %confirm 1 shows existance of that trial
+plot(hel_go) %confirm 1 shows existence of that trial
+hel_go_d=hel_go.*drift_parameters(1);
 hel_go=hel_go.*bias_parameters(1);
 
 hel_change=ismember(trial_sequence,'High Error Likelihood/CHANGE');
+hel_change_d=hel_change.*drift_parameters(2);
 hel_change=hel_change.*bias_parameters(2);
 
 lel_go = ismember(trial_sequence,'Low Error Likelihood/GO');
+lel_go_d=lel_go.*drift_parameters(3);
 lel_go=lel_go.*bias_parameters(3);
 
 lel_change = ismember(trial_sequence,'Low Error Likelihood/CHANGE');
+lel_change_d=lel_change.*drift_parameters(4);
 lel_change=lel_change.*bias_parameters(4);
 
 %prediction of the trial by trial change in the bias parameter
-activity_prediction_vector=hel_go+hel_change+lel_go+lel_change;
+activity_prediction_vector=hel_go+hel_change+lel_go+lel_change; %bias
+activity_prediction_vector_d=hel_go_d+hel_change_d+lel_go_d+lel_change_d; %drift
 
+figure(1), clf
 plot(activity_prediction_vector)
-%confirm that first trial is lel_change and that shows -0.3 bias
+%confirm that first trial is lel_go and that shows 0.472 bias
+plot(activity_prediction_vector_d)
 
 %now correlate/regress these against brain activity
