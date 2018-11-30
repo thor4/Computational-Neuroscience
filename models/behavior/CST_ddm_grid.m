c3f501858c3761760fb00a886aa74204fe0108e2
Bryan Conklin <bryan.conklin4@gmail.com>
2018-11-29 21:52:57 -0500

    update ddm parts 1 & 2 of hw 5_6

--- models/behavior/CST_ddm_grid.m
+++ models/behavior/CST_ddm_grid.m
@@ -1,29 +1,22 @@
 %% Drift diffusion model for change signal task (CST)
 % accuracy for the following 4 conditions of the CST:
 % high error likelihood go condition
-he_go_perf = .94; %accuracy
-he_go_rt = 264.9; %response time
+he_go_perf = .94; he_go_rt = 264.9; %accuracy & response time
 % high error likelihood change condition
-he_change_perf = .72; %accuracy
-he_change_rt = 387.8; %response time
+he_change_perf = .72; he_change_rt = 387.8; %accuracy & response time
 % low error likelihood go condition
-le_go_perf = .98; %accuracy
-le_go_rt = 220.4; %response time
+le_go_perf = .98; le_go_rt = 220.4; %accuracy & response time
 % low error likelihood change condition
-le_change_perf = .90; %accuracy
-le_change_rt = 293.8; %response time
+le_change_perf = .90; le_change_rt = 293.8; %accuracy & response time
 
 %all condition accuracies
 cond_perf = [he_go_perf he_change_perf le_go_perf le_change_perf];
 
 % try diff levels of the drift & bias params to see what effect this has on
 % accuracy
-% drift_range = -0.01:0.001:0.01;
-% bias_range = -0.7:0.1:0.7;
+% drift_range = -0.01:0.001:0.01; %coarse search
+% bias_range = -0.7:0.1:0.7; %coarse search
 drift_range = 0:0.001:0.005; %granular search
-
-% % zoom in on drift_range(11:20)
-% drift_range = 0:.0001:0.009;
 
 squared_distance_matrix = zeros(length(cond_perf),length(drift_range),length(bias_range));
 % correlation_matrix = squared_distance_matrix;
@@ -31,13 +24,13 @@ squared_distance_matrix = zeros(length(cond_perf),length(drift_range),length(bia
 tic
 for condN=1:4
     if condN==1
-        bias_range = 0.05:0.001:0.15;
+        bias_range = 0.05:0.001:0.15; %granular search
     elseif condN==2
-        bias_range = -0.35:0.001:-0.25;
+        bias_range = -0.35:0.001:-0.25; %granular search
     elseif condN==3
-        bias_range = 0.45:0.001:0.55;
+        bias_range = 0.45:0.001:0.55; %granular search
     else
-        bias_range = -0.15:0.001:-0.05; 
+        bias_range = -0.15:0.001:-0.05; %granular search
     end
     for dN=1:length(drift_range)
         for bN=1:length(bias_range)
@@ -58,21 +51,21 @@ end
 toc
 
 %% visualization
-figure(3), clf
+figure(5), clf
 subplot(221), imagesc(-log(squeeze(squared_distance_matrix(1,:,:))))
-set(gca,'clim',[0 20]), colorbar
+set(gca,'clim',[10 13]), colorbar
 xlabel('bias'), ylabel('drift'), title('high error likelihood go condition')
 subplot(222), imagesc(-log(squeeze(squared_distance_matrix(2,:,:))))
-set(gca,'clim',[0 20]), colorbar
+set(gca,'clim',[10 13]), colorbar
 xlabel('bias'), ylabel('drift'), title('high error likelihood change condition')
 subplot(223), imagesc(-log(squeeze(squared_distance_matrix(3,:,:))))
-set(gca,'clim',[0 20]), colorbar
+set(gca,'clim',[10 13]), colorbar
 xlabel('bias'), ylabel('drift'), title('low error likelihood go condition')
 subplot(224), imagesc(-log(squeeze(squared_distance_matrix(4,:,:))))
-set(gca,'clim',[0 20]), colorbar
+set(gca,'clim',[10 13]), colorbar
 xlabel('bias'), ylabel('drift'), title('low error likelihood change condition')
-export_fig param_space_cond_neg_log_transform.png -transparent % no background
-export_fig param_space_cond_neg_log_transform.pdf % export to pdf
+export_fig param_space_cond_neg_log_transform_granular.png -transparent % no background
+export_fig param_space_cond_neg_log_transform_granular.pdf % export to pdf
 % figure(2), clf
 % imagesc(correlation_matrix), colorbar
 
@@ -80,31 +73,10 @@ x=zeros(1,4); y=zeros(1,4); %init x & y coord of param space per cond
 for condN=1:4 %find best params where minimum is most likely to be
     [x(condN),y(condN)] = find((squeeze(squared_distance_matrix(condN,:,:))) == min(min(squeeze(squared_distance_matrix(condN,:,:)))));
 end
-drift_range(x); bias_range(y); % best drift & bias param
+drift_range(x) % best drift params
+% best bias params (cycle through 1-4 per condition's bias_range)
+bias_range = 0.05:0.001:0.15; bias_range(y(1)) %condition 1
+bias_range = -0.35:0.001:-0.25; bias_range(y(2)) %condition 2
+bias_range = 0.45:0.001:0.55; bias_range(y(3)) %condition 3
+bias_range = -0.15:0.001:-0.05; bias_range(y(4)) %condition 4
 
-%% setup table
-Condition = {'high error likelihood go'; 'high error likelihood change'; ...
-    'low error likelihood go'; 'low error likelihood change'};
-Drift = drift_range(x)'; Bias = bias_range(y)';
-T = table(Condition,Drift,Bias);
-writetable(T,'condition_params.txt');
-
-%% granular bias_range analysis
-[xcond1,ycond1] = find((squeeze(squared_distance_matrix(1,:,:))) == min(min(squeeze(squared_distance_matrix(1,:,:)))));
-[xcond1_neglog,ycond1_neglog] = find(((squeeze(squared_distance_matrix(1,:,:))).^.5) == min(min((squeeze(squared_distance_matrix(1,:,:))).^.5)));
-[xcond2,ycond2] = find((squeeze(squared_distance_matrix(2,:,:))) == min(min(squeeze(squared_distance_matrix(2,:,:)))));
-[xcond3,ycond3] = find((squeeze(squared_distance_matrix(3,:,:))) == min(min(squeeze(squared_distance_matrix(3,:,:)))));
-[xcond4,ycond4] = find((squeeze(squared_distance_matrix(4,:,:))) == min(min(squeeze(squared_distance_matrix(4,:,:)))));
-
-%pull out values from sq dist mat
-sq_cond1 = diag(squeeze(squared_distance_matrix(1,xcond1,ycond1)));
-sq_cond2 = diag(squeeze(squared_distance_matrix(2,xcond2,ycond2)));
-sq_cond3 = diag(squeeze(squared_distance_matrix(3,xcond3,ycond3)));
-sq_cond4 = diag(squeeze(squared_distance_matrix(4,xcond4,ycond4)));
-%confirmed- all minimums from each condition are 0
-
-%identify the best-fit drift and bias parameters
-d_cond1 = drift_range(xcond1); b_cond1 = bias_range(ycond1);
-d_cond2 = drift_range(xcond2); b_cond2 = bias_range(ycond2);
-d_cond3 = drift_range(xcond3); b_cond3 = bias_range(ycond3);
-d_cond4 = drift_range(xcond4); b_cond4 = bias_range(ycond4);
